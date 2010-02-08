2c0f327b7afb93bb56f099729a3af63f79605523
sousk.net <sou.phphp@gmail.com>
2010-02-09 08:50:26 +0900

    hand copying world clock widget

---
+++ samples/safari_ref/canvas.js
@@ -0,0 +1,26 @@
+// document.addEventListener('load', function() {
+document.load = function() {
+	function drawHands (hoursAngle, minutesAngle, secondsAngle)
+	{
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+	}
+};
+
+function log (msg) {
+	console.log(msg);
+}
+
+function $(id) {
+	return document.getElementById(id);
+}
+
+function load() {
+	// loadPreferences();
+	
+	// var doneButton = $('done');
+	// new AppleGlassButton (doneButton, getLocalizedString('Done'), doneClicked);	 	
+	// new AppleInfoButton($("infoButton"), $("front"), "white", "white", showbackside);
+	// 
+	// startClockTimer();
+}
---
+++ samples/safari_ref/native_dnd.js
@@ -0,0 +1,90 @@
+function log (msg) {
+	console.log(msg);
+}
+function fetch_data_transfer (evt) {
+	return evt.originalEvent.dataTransfer;
+}
+function log_effect_allowed (evt) {
+	log(fetch_data_transfer(evt).effectAllowed);
+}
+function mark_current_target (evt, targets) {
+	var klass = "starred";
+	if (targets.is(".starred")) {
+		targets.removeClass(klass);
+	}
+	if (evt) {
+		var tg = $(evt.currentTarget);
+		if (tg.is(".target")) {
+			tg.addClass(klass);
+		}
+	}
+}
+
+$(document).ready(function() {
+	
+	var 
+		drops = $('img, .draggable-txt'),
+		targets = $('.target'),
+		dragging = $('img').get(0);
+	
+	$(document).bind('dragstart', function(e) {
+		log("start 1");
+	  e.dataTransfer.setDragImage(dragging, 0, 0); // doesn't work
+	});
+	document.addEventListener('dragstart', function(evt) {
+		log("start 2");
+   	evt.dataTransfer.setDragImage(dragging, 0, 0); // works
+	});
+	document.ondragstart = function(e) {
+		log("start 3");
+	  e.dataTransfer.setDragImage(dragging, 0, 0); // works
+	};
+	return;
+	
+	drops
+		.bind('dragstart', function(evt) {
+			log("start");
+			log(evt);
+			// evt.originalEvent.dataTransfer.setDragImage(dragging.get(0),0,0);
+			// var obj = evt.originalEvent.dataTransfer;
+			// for(var k in obj) {
+			// 	log(k +"::"+obj[k]);
+			// }
+			// return false; stops dragging
+		})
+		.bind('drag', function(evt) {
+			// log("dragging, count-up while moving");
+			return false; // no meaning
+		})
+		.bind('dragend', function(evt) {
+			log("dragend");
+			mark_current_target(null, targets);
+			return false; // no meaning
+		});
+	
+	targets
+		.bind('dragover', function(evt) {
+			// mark_current_target(evt, targets);
+			log("count-up while overed");
+			// shows [+] button
+			return false;
+		})
+		.bind('dragenter', function(evt) {
+			mark_current_target(evt, targets);
+			log("entered");
+			return false;
+		})
+		.bind('dragleave', function(evt) {
+			mark_current_target(null, targets);
+			log("leaved");
+			return false;
+		})
+		.bind('drop', function(evt) {
+			log("droped");
+			log(evt);
+			// drop.html(
+				 // drop.html() + '<p>' + evt.dataTransfer.getData('Text') + '</p>'
+			// );
+			// return false;
+		});
+});
